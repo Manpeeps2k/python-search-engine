f689af8dbc45ec3a94c13cd0ec9de5d094867b98
Manpeeps2k <216877517+Manpeeps2k@users.noreply.github.com.>
2026-06-12 23:20:59 +0800

    created and compared search items, added user input as well as ranking

--- main.py
+++ main.py
@@ -46,9 +46,29 @@ class vectorChecking:
                 con[word] = 1 #first instance
         return con
 
+#parsed data
+documents = {}
+
+#indexing data to dictionary
+index = {
+
+}
+
+v = vectorChecking()
+
+inputSearching = input('Input Search: ')
+matches = []
+
+for i in range(len(index)):
+    relation = relation(v.word_count(inputSearching), index[i])
+    if relation != 0:
+        matches.append((relation,documents[i][:200]))
+
+#ranking
+matches.sort(reverse=True)
 
 
+#displaying results
+print(f"pending matches\n {matches[0]}\n{matches[1]}")
 
-
-# Indexing
 
